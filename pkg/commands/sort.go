76a91b4a4c3f762c8a35405ea3f57b69f3dc4d4a
Fabien Couillard <couillard.fabien+dev@gmail.com>
2026-08-02 23:59:12 +0200

    fix: correct conditional return placement in `sort` command to ensure proper flow

--- pkg/commands/sort.go
+++ pkg/commands/sort.go
@@ -155,8 +155,8 @@ func handleFile(path string, configs []datatype.ConfigEntity, db *gorm.DB, move 
 			if utils.MoveAndCheckFile(src, dst, newVideo.Name) {
 				newVideo.Path = dst
 				persistVideo(newVideo, db)
-				return
 			}
+			return
 		}
 	}
 
